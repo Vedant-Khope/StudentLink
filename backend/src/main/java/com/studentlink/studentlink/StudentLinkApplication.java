7e1fb7a24c1b4331a8b88a34d8b67a5144f34d0c
Vedant Khope <vedantkhope59@gmail.com>
2025-08-01 13:11:06 +0530

    chore: set up backend & frontend using Spring Initializr & Vite

---
+++ backend/src/main/java/com/studentlink/studentlink/StudentLinkApplication.java
@@ -0,0 +1,13 @@
+package com.studentlink.studentlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLinkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/studentlink/studentlink/StudentLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentlink.studentlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
